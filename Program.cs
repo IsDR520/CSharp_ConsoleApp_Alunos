56b2f1242ac263e77f54a7d40df1e12484756aa9
IsDR520 <is_reis_sp@hotmail.com>
2021-02-08 17:01:34 -0300

    Novo aluno e listar aluno implementado

--- Program.cs
+++ Program.cs
@@ -6,6 +6,52 @@ namespace Revisao
     {
         static void Main(string[] args)
         {
+            Aluno[] alunos = new Aluno[5];
+            var indiceAluno = 0;
+            string opcaoUsuario = ObterOpcaoUsusario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        Console.WriteLine("Informe o nome do aluno:");
+                        Aluno aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine("Informe a nota do aluno: ");
+                        if (decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                            aluno.Nota = nota;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("Valor da nota deve ser decimal");
+                        }
+
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
+
+                        break;
+                    case "2":
+                        foreach (var a in alunos)
+                        {
+                            if (string.IsNullOrEmpty(a.Nome))
+                            { Console.WriteLine($"Aluno: {a.Nome} - NOTA: {a.Nota}"); }
+                        };
+                        break;
+                    case "3":
+                        //TODO:calcular media geral
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                opcaoUsuario = ObterOpcaoUsusario();
+            }
+        }
+
+        private static string ObterOpcaoUsusario()
+        {
             Console.WriteLine("Informe a opção desejada: ");
             Console.WriteLine("1 - Inserir novo Aluno");
             Console.WriteLine("2 - Listar Alunos");
@@ -14,34 +60,8 @@ namespace Revisao
             Console.WriteLine();
 
             string opcaoUsuario = Console.ReadLine();
-
-            while (opcaoUsuario.ToUpper() != "X")
-            {
-                switch (opcaoUsuario)
-                {
-                    case "1":
-                        //TODO: Adicionar aluno
-                        break;
-                    case "2":
-                        //TODO: listar alunos
-                        break;
-                    case "3":
-                        //TODO:calcular media geral
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
-
-                Console.WriteLine("Informe a opção desejada: ");
-                Console.WriteLine("1 - Inserir novo Aluno");
-                Console.WriteLine("2 - Listar Alunos");
-                Console.WriteLine("3 - calcular média geral");
-                Console.WriteLine("X - Sair");
-                Console.WriteLine();
-
-                opcaoUsuario = Console.ReadLine();
-            }
-
+            Console.WriteLine();
+            return opcaoUsuario;
         }
     }
 }
